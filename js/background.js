2f2b88858d9e1bc211d4d26bd14f824fb857b2a6
Luis Carlos Limas <luiscla27@hotmail.com>
2026-02-12 01:30:25 -0600

    extension manifest.json (#6)

--- js/background.js
+++ js/background.js
@@ -17,9 +17,50 @@ chrome.runtime.onInstalled.addListener(function() {
 });
 
 
+
 chrome.action.onClicked.addListener(function(tab) {
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     files: ['js/main.js']
   });
+});
+
+// Create the context menu
+chrome.contextMenus.create({
+  id: "sponsor",
+  title: "Sponsor",
+  contexts: ["action"]
+});
+// Create the context menu
+chrome.contextMenus.create({
+  id: "donate",
+  title: "Donate",
+  contexts: ["action"]
+});
+chrome.contextMenus.create({
+  id: "buy-me-a-coffee",
+  title: "Buy me a coffee",
+  contexts: ["action"]
+});
+
+// Handle context menu clicks
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "sponsor") {
+    // Open the funding page in a new tab
+    chrome.tabs.create({
+      url: "https://github.com/sponsors/luiscla27"
+    });
+  }
+  if (info.menuItemId === "donate") {
+    // Open the funding page in a new tab
+    chrome.tabs.create({
+      url: "https://www.paypal.com/paypalme/luiscla27"
+    });
+  }
+  if (info.menuItemId === "buy-me-a-coffee") {
+    // Open the funding page in a new tab
+    chrome.tabs.create({
+      url: "https://buymeacoffee.com/luiscla27"
+    });
+  }
 });
