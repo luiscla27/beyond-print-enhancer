1ceff0476d24d742158d8ef6013408edd43e317e
luiscla27 <luiscla27@hotmail.com>
2026-02-23 21:24:40 -0600

    conductor(checkpoint): Checkpoint end of Phase 1: Infrastructure & Assets

--- test/unit/dom_manager.test.js
+++ test/unit/dom_manager.test.js
@@ -201,11 +201,6 @@ describe('DomManager & ElementWrapper', () => {
                 const manager = DomManager.getInstance();
                 assert.strictEqual(manager.selectors.UI.SHAPE_CONTAINER, '.print-shape-container');
             });
-
-            it('should have wrapper selector', () => {
-                const manager = DomManager.getInstance();
-                assert.strictEqual(manager.selectors.UI.WRAPPER, '.be-section-wrapper');
-            });
         });
     });
 });
